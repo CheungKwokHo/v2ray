7389c39399c2345d1eec991d03b88a312b3573d0
CheungKwokHo <cheungkwokho@hotmail.com>
2020-05-02 00:49:19 +0800

    Initial Commit

--- tools/v1xx_to_v3xx.sh
+++ tools/v1xx_to_v3xx.sh
@@ -34,80 +34,60 @@ cat >/etc/v2ray/adam_v2ray_backup.conf <<-EOF
 # 警告...请不要修改或删除这个文件...谢谢
 # 警告...请不要修改或删除这个文件...谢谢
 # -----------------------------------
-
+# ---- 再次提醒 ----
+# 大胸弟...如果你看到了这个...记得不要修改或更改这个文件
+# ---- 说明 ----
+# 嗯……这个文件呢，是用来备份一些设置的
+#
 #mark=v3
 #
 #
-
 # ---- V2Ray 传输协议 -----
 v2ray_transport=$v2ray_transport
-
 #---- V2Ray 端口 -----
 v2ray_port=$v2ray_port
-
 #---- UUID -----
 v2ray_id=$v2ray_id
-
 #---- alterId -----
-alterId=64
-
+alterId=233
 #---- V2Ray 动态端口开始 -----
 v2ray_dynamicPort_start=$v2ray_dynamicPort_start
-
 #---- V2Ray 动态端口结束 -----
 v2ray_dynamicPort_end=$v2ray_dynamicPort_end
-
 #---- 域名 -----
 domain=$domain
-
 #---- caddy -----
 caddy_status=$caddy_installed
-
 #---- Shadowsocks -----
 shadowsocks_status=$shadowsocks
-
 #---- Shadowsocks 端口 -----
 ssport=$ssport
-
 #---- Shadowsocks 密码 -----
 sspass=$sspass
-
 #---- Shadowsocks 加密协议 -----
 ssciphers=$ssciphers
-
 #---- 屏蔽广告 -----
 blocked_ad_status=$is_blocked_ad
-
 #---- 网站伪装 -----
 path_status=$is_ws_path
-
 #---- 伪装的路径 -----
 path=$ws_path
-
 #---- 伪装的网址 -----
 proxy_site=$proxy_site
-
 #---- Socks -----
 socks=
-
 #---- Socks 端口-----
-socks_port=6666
-
+socks_port=233
 #---- Socks 用户名 -----
-socks_username=adam
-
+socks_username=233blog
 #---- Socks 密码 -----
-socks_userpass=adamz.cc
-
+socks_userpass=233blog.com
 #---- MTProto -----
 mtproto=
-
 #---- MTProto 端口-----
-mtproto_port=6666
-
+mtproto_port=233
 #---- MTProto 用户密钥 -----
-mtproto_secret=adamz.cc
-
+mtproto_secret=lalala
 #---- 屏蔽 BT -----
 ban_bt=true
 		EOF
@@ -119,6 +99,8 @@ if [[ -f /usr/local/bin/v2ray ]]; then
 fi
 
 echo
-echo -e "\n $yellow 警告: 请重新登录 SSH 以避免出现 v2ray 命令未找到的情况...$none  \n" && exit 1
+echo -e " 哇哦...脚本差点就跪了..."
+echo
+echo -e "\n $yellow 警告: 请重新登录 SSH 以避免出现 v2ray 命令未找到的情况。$none  \n" && exit 1
 echo
 exit 1
